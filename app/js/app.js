cacab711ef411aaaf0c2d8622fd645c827d15e8d
“Aleksandr <kaiot1984@gmail.com>
2024-02-10 18:29:13 +0300

    create home page

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,13 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
 import Swiper from 'swiper'
+import {Navigation, Pagination} from 'swiper/modules';
 
 document.addEventListener('DOMContentLoaded', () => {
-
-    const swiper = new Swiper('.swiper', {
+    // home-top
+    const homeTop = new Swiper('.home-top-swiper', {
         // Optional parameters
+        modules: [Navigation, Pagination],
         direction: 'horizontal',
         loop: true,
 
@@ -16,14 +18,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Navigation arrows
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
+            nextEl: '.home-top__slider-next',
+            prevEl: '.home-top__slider-pre',
         },
     });
 
+    // home-gallery
+    const homeGallery = new Swiper('.swiper-gallery', {
+        direction: 'horizontal',
+        slidesPerView: 3,
+        loop: true,
+        loopAdditionalSlides: 3,
+        loopedSlides: 3,
+        spaceBetween: 10,
+    });
+
+    // home-feedback
+    const homeFeedback = new Swiper('.home-feedback-swiper', {
+        // Optional parameters
+        modules: [Pagination],
+        direction: 'horizontal',
+        slidesPerView: 4,
+        loop: true,
+        loopAdditionalSlides: 4,
+        loopedSlides: 4,
+        spaceBetween: 30,
+
+        // If we need pagination
+        pagination: {
+            el: '.home-feedback-swiper-pagination',
+        },
+    });
 })
